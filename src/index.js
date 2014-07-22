b090b067f98207337985ce237d533c0481cc8011
Bruno Assarisse <bruno@assarisse.com.br>
2014-07-22 11:57:00 -0300

    Exporting mssql.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ var Monologue = require( 'monologue.js' );
 var machina = require( 'machina' )();
 var SqlContext = require( '../src/sqlContext.js' )( sql, Monologue, machina );
 var TransactionContext = require( '../src/transactionContext.js' )( sql, SqlContext );
-var seriate = require( '../src/main.js' )( SqlContext, TransactionContext );
+var seriate = require( '../src/main.js' )( sql, SqlContext, TransactionContext );
 
 module.exports = seriate;
