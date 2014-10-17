2de64a94021a22acba04e8e27288e87c6d165bfb
Jim Cowart <jim@ifandelse.com>
2014-10-17 00:20:27 -0400

    Added callsite as a dep, and refactored fromFile to support referencing sql files relative to the caller and still resolve the path, etc.

--- spec/unit/external-sql.spec.js
+++ spec/unit/external-sql.spec.js
@@ -13,17 +13,17 @@ describe( 'When calling seriate.fromFile', function() {
 	var expected = "SELECT\t [Id]\n\t\t,[Title]\n\t\t,[Description]\n\t\t,[ClassOfServiceEnabled]\n\t\t,[OrganizationId]\nFROM\t[dbo].[Board]\nWHERE\tid = @id\n";
 	describe( "when loading a file using the extension with file name", function() {
 		it( "should load expected sql from file", function() {
-			expect( sql.fromFile( path.join( __dirname, "./test.sql" ) ) ).to.eql( expected );
+			expect( sql.fromFile( "./test.sql" ) ).to.eql( expected );
 		} );
 	} );
 	describe( "when loading a file without using the extension", function() {
 		it( "should load expected sql from file", function() {
-			expect( sql.fromFile( path.join( __dirname, "./test" ) ) ).to.eql( expected );
+			expect( sql.fromFile( "./test" ) ).to.eql( expected );
 		} );
 	} );
 	describe( "when loading a file where file name ends with period", function() {
 		it( "should load expected sql from file", function() {
-			expect( sql.fromFile( path.join( __dirname, "./test." ) ) ).to.eql( expected );
+			expect( sql.fromFile( "./test." ) ).to.eql( expected );
 		} );
 	} );
 } );
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ var fs = require( "fs" );
 var path = require( "path" );
 var SqlContext;
 var TransactionContext;
+var callsite = require( "callsite" );
 
 function promisify( context, queryOptions ) {
 	context.step( "result", queryOptions );
@@ -12,6 +13,10 @@ function promisify( context, queryOptions ) {
 				.error( reject )
 				.on( "data", notify );
 		} );
+}
+
+function isAbsolutePath( p ) {
+	return path.resolve( p ) === path.normalize( p ).replace( /(.+)([\/|\\])$/, "$1" );
 }
 
 module.exports = function( SqlContextCtor, TransactionContextCtor ) {
@@ -39,6 +44,14 @@ module.exports = function( SqlContextCtor, TransactionContextCtor ) {
 				} ), queryOptions );
 		},
 		fromFile: function( p ) {
+			// If we're not dealing with an absolute path, then we
+			// need to get the *calling* code's directory, since
+			// the sql file is being referenced relative to that location
+			if ( !isAbsolutePath( p ) ) {
+				var stack = callsite();
+				var requester = stack[ 1 ].getFileName();
+				p = path.join( path.dirname( requester ), p );
+			}
 			var ext = path.extname( p );
 			p = ( ext === "." ) ? ( p + "sql" ) : ( ext.length === 0 ) ? p + ".sql" : p;
 			return fs.readFileSync( p, { encoding: "utf8" } );
