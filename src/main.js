63d5c5eddb7816261dc4bd71183b383cc94f0783
Brian Edgerton <brian.edgerton@leankit.com>
2014-11-10 11:56:31 -0500

    Split up some file resolution logic so it could be leveraged for mocking.

--- src/main.js
+++ src/main.js
@@ -62,15 +62,20 @@ module.exports = function( SqlContextCtor, TransactionContextCtor ) {
 				return rows[ 0 ];
 			} );
 		},
-		fromFile: function( p ) {
+		_getFilePath: function( p ) {
 			// If we're not dealing with an absolute path, then we
 			// need to get the *calling* code's directory, since
 			// the sql file is being referenced relative to that location
 			if ( !isAbsolutePath( p ) ) {
 				var stack = callsite();
-				var requester = stack[ 1 ].getFileName();
+				var requester = stack[ 2 ].getFileName();
 				p = path.join( path.dirname( requester ), p );
 			}
+
+			return p;
+		},
+		fromFile: function( p ) {
+			p = this._getFilePath( p );
 			var ext = path.extname( p );
 			p = ( ext === "." ) ? ( p + "sql" ) : ( ext.length === 0 ) ? p + ".sql" : p;
 			return fs.readFileSync( p, { encoding: "utf8" } );
